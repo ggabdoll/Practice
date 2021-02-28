7251a76e75a4a56646d9c1c0bd5e65e49f009ee2
ggabdoll <ggabdoll0327@naver.com>
2021-03-01 04:29:00 +0900

    Api 04

---
+++ method13/Api04.java
@@ -0,0 +1,28 @@
+package method13;
+
+import java.util.Vector;
+
+public class Api04 {
+
+	public Vector<String>moveToVector(String[] datas){
+		Vector<String> v = new Vector<String>(); 
+		
+		for(int call = datas.length-1; call >=0; call-- ) {
+			v.addElement(datas[call]);
+		}
+		
+		return v;
+	}
+	
+	
+	
+	
+	
+	
+	public static void main(String[] args) {
+		Api04 ac = new Api04();
+		String datas[] = {"1","2","3","4","5"};
+		System.out.println(ac.moveToVector(datas));
+	}
+
+}
