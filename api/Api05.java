27f5deb8769e3a37bd75693faeea8b4198a83c24
ggabdoll <ggabdoll0327@naver.com>
2021-03-01 01:32:23 +0900

    Api 05

--- api/Api05.java
+++ api/Api05.java
@@ -3,8 +3,32 @@ package api;
 public class Api05 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		String[] arr = {"java program", "array","java program area","append"};
+		String result = Api05.replaceString(arr);
+		System.out.println("변경된 문자열: " +result);	
 	}
 
-}
+		private static String replaceString(String[]arr) {
+			String path;
+			int index =0, aplus =0 , max =0;
+			
+			for(int i = 0; i < arr.length;i++) {
+				aplus = 0;
+				
+				for(int j = 0; j < arr[i].length();j++) {
+					if(arr[i].charAt(j) == 'a') {
+						aplus ++;
+					}
+				}
+				if(aplus >= max) {
+					max = aplus;
+					index = i;
+				}
+			}
+			
+			path = arr[index].replace('a','A');
+			return path;
+		}
+	}
+
