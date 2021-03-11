8aa982483a689b618f24dcdea484e0dd9c3c29c2
ggabdoll <ggabdoll0327@naver.com>
2021-03-11 23:35:54 +0900

    21.3.11

---
+++ Backjoon/Selfnumber.java
@@ -0,0 +1,22 @@
+package Backjoon;
+
+public class Selfnumber {
+	
+	int n ;
+	
+	public int sell(int d) {
+		
+		String n = String.valueOf(d); 
+		
+		
+		
+		return 0;
+	}
+	
+	public static void main(String args[]) {
+		Selfnumber  sf = new Selfnumber();
+		
+		System.out.println(sf);
+	}
+
+}
