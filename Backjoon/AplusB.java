7861de75ca48a8e869ec2613ab6aa468f65b56af
ggabdoll <ggabdoll0327@naver.com>
2021-03-15 20:39:05 +0900

    21.3.15

---
+++ Backjoon/AplusB.java
@@ -0,0 +1,32 @@
+package Backjoon;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class AplusB {
+
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int N = Integer.parseInt(br.readLine());
+		
+		StringTokenizer st;
+		
+		
+		for(int i =1; i <= N; i++) {
+			st = new StringTokenizer(br.readLine()," ");
+			bw.write("Case #"+i+": ");
+			bw.write((Integer.parseInt(st.nextToken()) + Integer.parseInt(st.nextToken())) + "\n");
+			}
+		br.close();
+		bw.flush();
+		bw.close();
+	}
+
+}
