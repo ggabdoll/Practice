e901587ba26d563ced8200241962ded5dbbe234b
ggabdoll <ggabdoll0327@naver.com>
2021-03-15 19:49:08 +0900

    21.3.15

---
+++ Backjoon/SpeedAplusB.java
@@ -0,0 +1,30 @@
+package Backjoon;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class SpeedAplusB {
+
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		int in = Integer.parseInt(br.readLine());
+		
+		StringTokenizer st;
+		
+		for(int i = 0; i < in; i++) {
+			st = new StringTokenizer(br.readLine()," ");
+			bw.write((Integer.parseInt(st.nextToken()) + Integer.parseInt(st.nextToken())) + "\n");
+			}
+		br.close();
+		
+		bw.flush();
+		bw.close();
+	}
+
+}
