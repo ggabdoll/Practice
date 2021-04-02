fe9678684d0154e0071e78c2b6d18d29da3a0cc1
ggabdoll <ggabdoll0327@naver.com>
2021-04-02 21:22:33 +0900

    21.4.2

---
+++ Backjoon/DeliverSuger.java
@@ -0,0 +1,41 @@
+package Backjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class DeliverSuger {
+
+	public static void main(String[] args) throws IOException{
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
+		
+		
+		if (N % 5 == 0) {
+			System.out.println(N / 5);
+		
+		}else if (N % 5 !=0 && N > 6) {
+			int small = (N%5)/3;
+			
+			if(small == 0) {
+				
+				System.out.println((N%5) + small);
+			
+			}else {
+				System.out.println(-1);
+			}
+			
+		}
+		
+		if(N<=6 && N%3 == 0) {
+			System.out.println(N/3);
+		}else if(N <=6 && N%3 != 0) {
+			System.out.println(-1);
+		}
+		}
+
+	}
+
+
