74ce74fb5152b0d551f53e7dfbcd19fdbeb9d862
ggabdoll <ggabdoll0327@naver.com>
2021-04-02 21:35:43 +0900

    21.4.2

--- Backjoon/DeliverSuger.java
+++ Backjoon/DeliverSuger.java
@@ -17,11 +17,12 @@ public class DeliverSuger {
 			System.out.println(N / 5);
 		
 		}else if (N % 5 !=0 && N > 6) {
-			int small = (N%5)/3;
+			int small = (N%5);
 			
-			if(small == 0) {
+			
+			if(small%3 == 0) {
 				
-				System.out.println((N%5) + small);
+				System.out.println((N%5) + (small/3));
 			
 			}else {
 				System.out.println(-1);
@@ -29,11 +30,11 @@ public class DeliverSuger {
 			
 		}
 		
-		if(N<=6 && N%3 == 0) {
-			System.out.println(N/3);
-		}else if(N <=6 && N%3 != 0) {
-			System.out.println(-1);
-		}
+//		if(N<=6 && N%3 == 0) {
+//			System.out.println(N/3);
+//		}else if(N <=6 && N%3 != 0) {
+//			System.out.println(-1);
+//		}
 		}
 
 	}
