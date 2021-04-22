9aa47c02f6a55736d4abc3c65fa73edfcf5940d7
ggabdoll <ggabdoll0327@naver.com>
2021-04-22 23:52:16 +0900

    21.4.22

---
+++ Backjoon/Sortinside.java
@@ -0,0 +1,28 @@
+package Backjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Sortinside {
+
+	public static void main(String[] args) throws IOException{
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		 
+		int[] counting = new int[10];
+		String s = br.readLine();
+ 
+		for (int i = 0; i < s.length(); i++) {
+			counting[s.charAt(i) - '0']++;
+		}
+ 
+		for (int i = 9; i >= 0; i--) {
+			while (counting[i]-- > 0) {
+				System.out.print(i);
+			}
+		}
+		
+	}
+
+}
