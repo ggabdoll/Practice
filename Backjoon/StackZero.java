644934cbbc6c7d124ed470a80daf77da6c2f130c
ggabdoll <ggabdoll0327@naver.com>
2021-06-05 19:29:43 +0900

    21.6.05

---
+++ Backjoon/StackZero.java
@@ -0,0 +1,35 @@
+package Backjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class StackZero {
+
+    public static void main(String[] args)throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        Stack<Integer> stack = new Stack<Integer>();
+
+        int K = Integer.parseInt(br.readLine());
+
+        for(int i =0; i < K; i++){
+            int number = Integer.parseInt(br.readLine());
+
+            if(number == 0){
+                stack.pop();
+            }
+            else {
+                stack.push(number);
+            }
+        }
+        int sum = 0;
+
+        for(int o : stack){
+            sum += o;
+        }
+
+        System.out.println(sum);
+    }
+}
