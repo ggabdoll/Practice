b1349ddf3473609fec515994ee895a7eb5ed4279
ggabdoll <ggabdoll0327@naver.com>
2021-06-06 14:32:28 +0900

    21.6.6

--- Backjoon/StackBracket1.java
+++ Backjoon/StackBracket1.java
@@ -1,2 +1,44 @@
-package Backjoon;public class StackBracket1 {
+package Backjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class StackBracket1 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int T = Integer.parseInt(br.readLine());
+
+        while (T-- > 0) {
+            sb.append(slove(br.readLine())).append('\n');
+        }
+        System.out.println(sb);
+    }
+
+    public static String slove(String s) {
+
+        int count = 0;
+
+        for (char c : s.toCharArray()) {
+
+            if (c == '(') {
+                count++;
+            } else if (count == 0) {
+                return "NO";
+            } else {
+                count--;
+            }
+        }
+
+        if(count == 0){
+            return "YES";
+        }
+        else {
+            return "NO";
+        }
+    }
 }
